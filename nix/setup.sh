953438c55f2b405b8f8c1636db8310025a288907
Tyler Dodge <tyler@tdodge.consulting>
2022-05-12 13:22:29 -0700

    Switch to new local build setup

--- nix/setup.sh
+++ nix/setup.sh
@@ -7,8 +7,13 @@ function buildPhase() {
     cp -r $test_target test
     chmod -R u+w test 
     ${emacs}/bin/emacs -q --version
-    yes yes | ${emacs}/bin/emacs -q -batch -l $company_tailwindcss -l ert-runner
-    mkdir $out
+    yes yes | ${emacs}/bin/emacs -q -batch -l $company_tailwindcss -l ert-runner | tee $out
+    STATUS="${PIPESTATUS[1]}"
+    if [ $STATUS -gt 0 ]
+    then
+        rm $out
+        exit $STATUS
+    fi
 }
 
 function genericBuild() {
